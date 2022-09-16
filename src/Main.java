a053284e06cc8aaf3834cb42eb766c057aa5df7a
RyanCap03 <capre@mail.uc.edu>
2022-09-15 23:13:38 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int num = 50;
+
+        if((num % 2) == 0)
+        {
+            System.out.println("Your number is even");
+        }
+        else
+        {
+            System.out.println("your number is odd");
+        }
+
+
+    }
+}
